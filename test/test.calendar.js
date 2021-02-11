bc58c992e488770687daf6e340501ac2493c9e99
arjyboy <arjunthakur2314@gmail.com>
2021-02-12 10:12:37 +1300

    Re-installed packages

--- test/test.calendar.js
+++ test/test.calendar.js
@@ -1,9 +1,8 @@
 var assert = require('assert');
 const customDate = require('../src/calendar.js')._setDateTitle;
 
-
 describe('App', function(){
-    it('Should return the type as of string',function(){
+    it('Should return the type of string',function(){
         let thing = _setDateTitle();
         assert.typeOf(thing, 'string');
 
